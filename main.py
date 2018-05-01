ac2cf9dd34fd288f608310a0909af2de6f65db1e
Mike Bilter <Mike.Bilter@gmail.com>
2018-04-30 21:35:47 -0400

    Added spell damage and take damage functions

---
+++ main.py
@@ -0,0 +1,9 @@
+from classes.game import Person, bcolors
+
+magic = [{'name': 'Fire', 'cost': '10', 'dmg': 60},
+         {'name': 'Thunder', 'cost': '15', 'dmg': 80},
+         {'name': 'Blizzard', 'cost': '20', 'dmg': 100}]
+
+
+player = Person(460, 65, 60, 34, magic)
+
