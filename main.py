4c9164e0d4d7a38cd858e3e97f4c203ca696d225
Mike Bilter <Mike.Bilter@gmail.com>
2018-05-01 16:07:39 -0400

    White magic now heals. Its possible to win!

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from classes.game import Person, bcolors
+from classes.magic import Spell
 
-magic = [{'name': 'Fire', 'cost': '10', 'dmg': 160},
-         {'name': 'Thunder', 'cost': '15', 'dmg': 180},
-         {'name': 'Blizzard', 'cost': '20', 'dmg': 200}]
+# Create Black Magic
+# spell_name = Spell('name', 'cost', 'dmg', 'type')
+fire = Spell('Fire', 10, 100, 'black')
+thunder = Spell('Thunder', 10, 100, 'black')
+blizzard = Spell('Blizzard', 10, 100, 'black')
+meteor = Spell('Meteor', 20, 200, 'black')
+quake = Spell('Quake', 14, 140, 'black')
 
+# Create White Magic
+# spell_name = Spell('name', 'cost', 'dmg', 'type')
+cure = Spell('Cure', 12, 120, 'white')
+cura = Spell('Cura', 18, 200, 'white')
 
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 25, magic)
+# Create Pawns
+# entity_name = Spell('hp', 'mp', 'atk', 'df', magic)
+player = Person(460, 65, 60, 34, [fire, thunder,blizzard, meteor, cure, cura])
+enemy = Person(1200, 65, 45, 25, [])
 
 running = True
-i = 0
 
 print(bcolors.FAIL + bcolors.BOLD + 'AN ENEMY ATTACKS!' + bcolors.ENDC)
 while running:
@@ -25,20 +35,24 @@ while running:
   elif index == 1:
     player.choose_magic()
     magic_choice = int(input('Choose magic: ')) - 1
-    magic_dmg = player.generate_spell_damage(magic_choice)
-    spell = player.get_spell_name(magic_choice)
-    cost = int(player.get_spell_mp_cost(magic_choice))
+
+    spell = player.magic[magic_choice]
+    magic_dmg = spell.generate_damage()
 
     current_mp = player.get_mp()
 
-    if cost > current_mp:
+    if spell.cost > current_mp:
       print(bcolors.FAIL + '\nNot enough MP\n' + bcolors.ENDC)
       continue
 
-    player.reduce_mp(cost)
-    enemy.take_damage(magic_dmg)
-    print(bcolors.OKBLUE + '\n' + spell + ' deals', str(magic_dmg), 'points of damage.' + bcolors.ENDC)
+    player.reduce_mp(spell.cost)
 
+    if spell.type == 'white':
+      player.heal(magic_dmg)
+      print(bcolors.OKBLUE + '\n' + spell.name + ' heals for', str(magic_dmg), 'HP.' + bcolors.ENDC)
+    elif spell.type == 'black':
+      enemy.take_damage(magic_dmg)
+      print(bcolors.OKBLUE + '\n' + spell.name + ' deals', str(magic_dmg), 'points of damage.' + bcolors.ENDC)
 
   enemy_choice = 1
 
